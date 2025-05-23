0a0af9a2ff9380e1ebcc65ddae40e287dc4ea735
Mohd Kaif <124128116+btwitskaif69@users.noreply.github.com>
2025-05-24 02:29:42 +0530

    fix: add comments to clarify usage of <a> tags in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,12 @@ const Navbar = () => {
       {/* Desktop Navbar */}
       <NavBody>
         <NavbarLogo />
+        {/*
+          Assuming NavItems also uses <a> tags internally or you'll modify it
+          to simply render <a> tags for its items.
+        */}
         <NavItems items={navItems} onItemClick={closeMobileMenu} />
+        {/* NavbarButton uses href, which means it will act as a standard <a> tag */}
         <NavbarButton href="/subscribe">Subscribe</NavbarButton>
       </NavBody>
 
@@ -43,15 +48,16 @@ const Navbar = () => {
 
         <MobileNavMenu isOpen={mobileOpen} onClose={closeMobileMenu}>
           {navItems.map((item, idx) => (
-            <a
+            <a // This is the <a> tag you requested
               key={`mobile-link-${idx}`}
-              href={item.link}
+              href={item.link} // The href attribute is used for the link destination
               onClick={closeMobileMenu}
               className="w-full px-4 py-2 text-lg text-black dark:text-white hover:bg-gray-100 dark:hover:bg-neutral-800 rounded"
             >
               {item.name}
             </a>
           ))}
+          {/* NavbarButton uses href, which means it will act as a standard <a> tag */}
           <NavbarButton href="/subscribe" className="w-full mt-4">
             Subscribe
           </NavbarButton>
